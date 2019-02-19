a4ddf3b1ffb5a4f8eb8c681a9fbb9e555534e2c7
Randy LeVeque <rjl@uw.edu>
2019-02-18 21:17:40 -0800

    use variable sea level also in qinit at time t0
    Not normally needed, but should do for consistency and would allow
    setting t0 to after an earthquake has finished to have sea floor deformation
    already in first frame.

--- src/2d/shallow/qinit.f90
+++ src/2d/shallow/qinit.f90
@@ -2,7 +2,8 @@
 subroutine qinit(meqn,mbc,mx,my,xlower,ylower,dx,dy,q,maux,aux)
     
     use qinit_module, only: qinit_type,add_perturbation
-    use geoclaw_module, only: sea_level
+    use geoclaw_module, only: sea_level, variable_sea_level
+    use amr_module, only: t0  ! initial time
     
     implicit none
     
@@ -14,11 +15,20 @@ subroutine qinit(meqn,mbc,mx,my,xlower,ylower,dx,dy,q,maux,aux)
     
     ! Locals
     integer :: i,j,m
+    real(kind=8) :: vsea(1-mbc:mx+mbc,1-mbc:my+mbc)
     
     ! Set flat state based on sea_level
+    !     or possibly non-flat if variable_sea_level
+
+    if (variable_sea_level) then
+        call set_sea_level(mbc,mx,my,xlower,ylower,dx,dy,t0,vsea)
+      else
+        vsea = sea_level ! same value everywhere
+      endif
+
     q = 0.d0
     forall(i=1:mx, j=1:my)
-        q(1,i,j) = max(0.d0, sea_level - aux(1,i,j))
+        q(1,i,j) = max(0.d0, vsea(i,j) - aux(1,i,j))
     end forall
     
     ! Add perturbation to initial conditions
