a61fe748805174f619d64b320bbb4a9b674da558
Kyle Mandli <kyle.mandli@gmail.com>
2019-02-24 17:01:23 -0800

    Try and fix absolute import problem

--- tests/bowl_slosh/regression_tests.py
+++ tests/bowl_slosh/regression_tests.py
@@ -15,7 +15,7 @@ import numpy
 
 import clawpack.geoclaw.test as test
 import clawpack.geoclaw.topotools as topotools
-import make_fgmax_grid
+import make_fgmax_grid 
 
 
 class BowlSloshTest(test.GeoClawRegressionTest):
@@ -38,7 +38,6 @@ class BowlSloshTest(test.GeoClawRegressionTest):
         topo.write(os.path.join(self.temp_path, "bowl.topotype2"), \
                 topo_type=2, Z_format="%22.15e")
 
-        # from . import make_fgmax_grid 
         make_fgmax_grid.make_fgmax_grid1(self.temp_path)
 
 
