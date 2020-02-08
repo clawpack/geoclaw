736670a8b05f3f3afc9d8bb04d72cc81dca76635
Randy LeVeque <rjl@uw.edu>
2020-02-07 20:37:32 -0800

    major changes to fgmax*.f90 and fgmax_tools.py, copied from clawpack/new_features_for_v5.7.0@3f4da6007a7
    See that repository for additional commits, though much of it was developed
    elsewhere for recent projects.
    
    This includes refactoring how fgmax monitoring is done so that it is
    much faster for large sets of points, introducing point_style==4,
    improvements to python tools, etc.
    
    Not backward compatible, requires fgmax_grids list in setrun.py as list
    of FGMaxGrid objects.   Writes output to fgmaxNNNN.txt rather than to
    fort.FGN.valuemax and fort.FGN.aux1 (topo value now included in single file).

--- src/2d/shallow/fgmax_values.f90
+++ src/2d/shallow/fgmax_values.f90
@@ -1,13 +1,13 @@
 
 subroutine fgmax_values(mx,my,meqn,mbc,maux,q,aux,dx,dy, &
-                   xlower,ylower,mask_patch,values)
+                   xlower,ylower,i1,i2,j1,j2,values)
 
     ! Given a grid q (and aux if needed), set the elements of 
     !   values(mv,i,j)  for mv=1:FG_NUM_VAL
     ! to the desired values that will be output and/or monitored on
     ! the fixed grid(s).
     !
-    ! Only the elements for which mask_patch(i,j) == .true. need be set.
+    ! Only the elements with indices in [i1,i2,j1,j2] need be set.
 
     ! This library routine expects FG_NUM_VAL to be 1, 2, or 5 and sets:
     !   values(1,i,j) = h              (if FG_NUM_VAL >= 1)
@@ -28,55 +28,36 @@ subroutine fgmax_values(mx,my,meqn,mbc,maux,q,aux,dx,dy, &
     real(kind=8), intent(in) :: q(meqn, 1-mbc:mx+mbc, 1-mbc:my+mbc)
     real(kind=8), intent(in) :: aux(maux, 1-mbc:mx+mbc, 1-mbc:my+mbc)
     real(kind=8), intent(in) :: dx,dy,xlower,ylower
-    logical, intent(in) :: mask_patch(1-mbc:mx+mbc, 1-mbc:my+mbc)
+    integer, intent(in) :: i1,i2,j1,j2
     real(kind=8), intent(inout) :: values(FG_NUM_VAL, 1-mbc:mx+mbc, 1-mbc:my+mbc)
 
-    real(kind=8) :: s,hs,hss,s_dry_tol
-    real(kind=8), dimension(:,:), allocatable :: u,v,h
+    real(kind=8) :: s_dry_tol
+    integer :: i,j
 
-    allocate(h(1-mbc:mx+mbc, 1-mbc:my+mbc))
-    if (FG_NUM_VAL > 1) then
-        allocate(u(1-mbc:mx+mbc, 1-mbc:my+mbc), v(1-mbc:mx+mbc, 1-mbc:my+mbc))
-        endif
-
-    h = q(1,:,:)
-
-    where (mask_patch)
-        values(1,:,:) = h
-    endwhere
-
-    if (FG_NUM_VAL == 1) then
-        return
-        endif
-
-    ! compute velocities:
-    s_dry_tol = dry_tolerance
-    where (mask_patch .and. (h > s_dry_tol))
-        u = q(2,:,:) / h
-        v = q(3,:,:) / h
-    elsewhere
-        u = 0.
-        v = 0.
-    endwhere
-
-    where (mask_patch)
-        values(2,:,:) = sqrt(u**2 + v**2)             ! s,    speed
-    endwhere
-
-    if (FG_NUM_VAL == 2) then
-        return
-        endif
-
-    if (FG_NUM_VAL .ne. 5) then
+    if ((FG_NUM_VAL.ne.1) .and. (FG_NUM_VAL.ne.2) .and. (FG_NUM_VAL.ne.5)) then
         write(6,*) '*** Error -- expecting FG_NUM_VAL = 1, 2, or 5'
         write(6,*) '***   in fgmax_values, found FG_NUM_VAL = ',FG_NUM_VAL
         stop
         endif
-
-    where (mask_patch)
-        values(3,:,:) = h*values(2,:,:)               ! hs,   momentum
-        values(4,:,:) = values(3,:,:)*values(2,:,:)   ! hs^2, momentum flux
-        values(5,:,:) = -h                            ! to find min h
-    endwhere
+        
+    s_dry_tol = dry_tolerance
+    
+    do i=i1,i2
+        do j=j1,j2
+            values(1,i,j) = q(1,i,j)
+            if (FG_NUM_VAL > 1) then
+                if (q(1,i,j) > s_dry_tol) then
+                    values(2,i,j) = sqrt(q(2,i,j)**2+q(3,i,j)**2) / q(1,i,j)
+                  else
+                    values(2,i,j) = 0.d0
+                  endif
+                endif
+            if (FG_NUM_VAL > 2) then
+                values(3,i,j) = q(1,i,j)*values(2,i,j)       ! hs,   momentum
+                values(4,i,j) = values(3,i,j)*values(2,i,j)  ! hs^2, mom flux
+                values(5,i,j) = -q(1,i,j)                    ! for min h
+                endif
+            enddo
+        enddo
 
 end subroutine fgmax_values
