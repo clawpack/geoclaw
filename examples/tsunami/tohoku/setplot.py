ea806ea6d662ce2e6461f17b5e0fab96364416d8
donnaaboise <donnacalhoun@boisestate.edu>
2020-05-29 09:51:22 -0600

    (geoclaw/tohoku) Add tohoku example

---
+++ examples/tsunami/tohoku/setplot.py
@@ -0,0 +1,420 @@
+
+"""
+Set up the plot figures, axes, and items to be done for each frame.
+
+This module is imported by the plotting routines and then the
+function setplot is called to set the plot parameters.
+
+"""
+
+
+#--------------------------
+def setplot(plotdata):
+#--------------------------
+
+    """
+    Specify what is to be plotted at each frame.
+    Input:  plotdata, an instance of clawpack.visclaw.data.ClawPlotData.
+    Output: a modified version of plotdata.
+
+    """
+
+
+    from clawpack.visclaw import colormaps, geoplot
+    from numpy import linspace
+
+    plotdata.clearfigures()  # clear any old figures,axes,items data
+
+
+    # To plot gauge locations on pcolor or contour plot, use this as
+    # an afteraxis function:
+
+    def addgauges(current_data):
+        from clawpack.visclaw import gaugetools
+        gaugetools.plot_gauge_locations(current_data.plotdata, \
+             gaugenos='all', format_string='ko', add_labels=False)
+
+    def fixup(current_data):
+        import pylab
+        addgauges(current_data)
+        t = current_data.t
+        t = t / 3600.  # hours
+        pylab.title('Surface at %4.2f hours' % t, fontsize=20)
+        #pylab.xticks(fontsize=15)
+        #pylab.yticks(fontsize=15)
+
+    #-----------------------------------------
+    # Some global kml flags
+    #-----------------------------------------
+    plotdata.kml_index_fname = "Tohoku_2011"     # Name for .kmz and .kml files.
+    plotdata.kml_name = "Tohoku 2011"
+
+    plotdata.kml_starttime = [2011,3,11,5,46,0]  # [Y,M,D,H,M,S] (UTC)
+    plotdata.kml_tz_offset = -9     # offset to UTC
+
+    plotdata.kml_publish = "http://math.boisestate.edu/~calhoun/visclaw/GoogleEarth/kmz"
+
+    #-----------------------------------------
+    # Figure for pcolor plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Bathymetry', figno=0)
+    plotfigure.show = True   # Don't show this file in the html version
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('pcolor')
+    plotaxes.title = 'Surface'
+    plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+    plotaxes.xlimits = [132.0, 210.0]
+    plotaxes.ylimits = [9.0, 53.0]
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
+    #plotitem.plot_var = geoplot.surface
+    plotitem.show = True
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.pcolor_cmap = geoplot.tsunami_colormap
+    plotitem.pcolor_cmin = -0.2
+    plotitem.pcolor_cmax = 0.2
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 1
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
+    plotitem.show = True
+    plotitem.plot_var = geoplot.land
+    plotitem.pcolor_cmap = geoplot.land_colors
+    plotitem.pcolor_cmin = 0.0
+    plotitem.pcolor_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [1,1,0]
+    plotitem.patchedges_show = 1
+    plotaxes.xlimits = [202., 206.]
+    plotaxes.ylimits = [19., 21.]
+
+    # add contour lines of bathy if desired:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    plotitem.show = True
+    plotitem.plot_var = geoplot.topo
+    plotitem.contour_levels = linspace(-2000,0,5)
+    plotitem.amr_contour_colors = ['w']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid','linewidths':1}
+    plotitem.amr_contour_show = [1,0,0]
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+
+
+    #-----------------------------------------
+    # Figure for imshow plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='imshow', figno=1)
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('imshow')
+    plotaxes.title = 'Surface'
+    plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    # plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = -0.2
+    plotitem.imshow_cmax = 0.2
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 1
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 1
+    plotaxes.xlimits = 'auto'
+    plotaxes.ylimits = 'auto'
+
+    # add contour lines of bathy if desired:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    plotitem.show = True
+    plotitem.plot_var = geoplot.topo
+    plotitem.contour_levels = linspace(-2000,0,5)
+    plotitem.amr_contour_colors = ['y']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid','linewidths':2}
+    plotitem.amr_contour_show = [1,0,0]
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+
+
+    #-----------------------------------------
+    # Figure for zoom plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='zoom', figno=2)
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('imshow')
+    plotaxes.title = 'Surface'
+    plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    # plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = -0.2
+    plotitem.imshow_cmax = 0.2
+    plotitem.add_colorbar = True
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 1
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 100.0
+    plotitem.add_colorbar = False
+    plotitem.amr_celledges_show = [0,0,0]
+    plotitem.patchedges_show = 1
+    plotaxes.xlimits = [203.2, 203.8]
+    plotaxes.ylimits = [20.6, 21.3]
+
+    # add contour lines of bathy if desired:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    plotitem.show = False
+    plotitem.plot_var = geoplot.topo
+    plotitem.contour_levels = linspace(-2000,0,5)
+    plotitem.amr_contour_colors = ['y']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid','linewidths':2}
+    plotitem.amr_contour_show = [1,0,0]
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+
+
+    #-----------------------------------------
+    # Figure for zoom plot
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='zoom3', figno=3)
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('imshow')
+    plotaxes.title = 'Surface'
+    plotaxes.scaled = True
+
+    plotaxes.afteraxes = fixup
+
+    # Water
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    # plotitem.plot_var = geoplot.surface
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.imshow_cmap = geoplot.tsunami_colormap
+    plotitem.imshow_cmin = -0.2
+    plotitem.imshow_cmax = 0.2
+    plotitem.add_colorbar = True
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 1
+
+    # Land
+    plotitem = plotaxes.new_plotitem(plot_type='2d_imshow')
+    plotitem.plot_var = geoplot.land
+    plotitem.imshow_cmap = geoplot.land_colors
+    plotitem.imshow_cmin = 0.0
+    plotitem.imshow_cmax = 10.0
+    plotitem.add_colorbar = False
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 1
+    plotaxes.xlimits = [203.45, 203.58]
+    plotaxes.ylimits = [20.85, 20.95]
+
+    # add contour lines of bathy if desired:
+    plotitem = plotaxes.new_plotitem(plot_type='2d_contour')
+    #plotitem.show = False
+    plotitem.plot_var = geoplot.topo
+    #plotitem.contour_levels = linspace(-2000,0,5)
+    plotitem.contour_levels = linspace(0,8,9)
+    plotitem.amr_contour_colors = ['y']  # color on each level
+    plotitem.kwargs = {'linestyles':'solid','linewidths':2}
+    plotitem.amr_contour_show = [0,0,0,0,0,1]
+    plotitem.celledges_show = 0
+    plotitem.patchedges_show = 0
+
+
+    #-------------------------------------------------------------------
+    # Figure for KML files
+    #--------------------------------------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Sea Surface',figno=4)
+    plotfigure.show = True   # Don't show this file in the html version
+
+    plotfigure.kml_use_for_initial_view= True
+    plotfigure.use_for_kml = True
+
+    # Resolution : numcells = [39, 22]; maxlevel = 4
+    # Refinement in setrun.py : [5, 6, 4]
+    rcl = 2    # rcl*figsize = numcells
+    plotfigure.kml_dpi = rcl*5*6*4
+    plotfigure.kml_figsize = [19.5,11]
+
+    plotfigure.kml_tile_images = False
+
+    # LatLong box used for plotting
+    plotfigure.kml_xlimits = [132.0, 210.0]
+    plotfigure.kml_ylimits = [9.0, 53.0]
+
+    cmin = -0.2
+    cmax = 0.2
+    cmap = geoplot.googleearth_transparent
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes('pcolor')
+    plotitem = plotaxes.new_plotitem(plot_type='2d_pcolor')
+    plotitem.plot_var = geoplot.surface_or_depth
+    plotitem.pcolor_cmap = cmap
+    plotitem.pcolor_cmin = cmin
+    plotitem.pcolor_cmax = cmax
+
+    def kml_colorbar(filename):
+        geoplot.kml_build_colorbar(filename,
+                                   cmap,
+                                   cmin,cmax)
+
+    plotfigure.kml_colorbar = kml_colorbar
+
+
+    #-----------------------------------------
+    # Figures for gauges
+    #-----------------------------------------
+    plotfigure = plotdata.new_plotfigure(name='Surface', figno=300, \
+                    type='each_gauge')
+    plotfigure.clf_each_gauge = True
+
+    # Set up for axes in this figure:
+    plotaxes = plotfigure.new_plotaxes()
+    #plotaxes.axescmd = 'subplot(2,1,1)'
+    plotaxes.title = 'Surface'
+
+    # Plot surface as blue curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.plot_var = 3
+    plotitem.plotstyle = 'b-'
+    plotitem.kwargs = {'linewidth':2}
+
+    # Plot topo as green curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.show = False
+
+    def gaugetopo(current_data):
+        q = current_data.q
+        h = q[0,:]
+        eta = q[3,:]
+        topo = eta - h
+        return topo
+
+    plotitem.plot_var = gaugetopo
+    plotitem.plotstyle = 'g-'
+
+    def add_zeroline(current_data):
+        from pylab import plot, legend, xticks, floor, xlim,ylim
+        t = current_data.t
+        #legend(('surface','topography'),loc='lower left')
+        plot(t, 0*t, 'k')
+        #n = int(floor(t.max()/1800.)) + 2
+        #xticks([1800*i for i in range(n)],[str(0.5*i) for i in range(n)])
+        #xlim(25000,t.max())
+        #ylim(-0.5,0.5)
+        print("+++ gaugeno = ",current_data.gaugeno)
+
+    def add_legend_eta(current_data):
+        from pylab import legend
+        legend(('Surface'),loc='lower left')
+        add_zeroline(current_data)
+
+    plotaxes.ylimits = [-2.5, 2.5]
+    plotaxes.afteraxes = add_zeroline
+
+
+    plotfigure = plotdata.new_plotfigure(name='Velocities', figno=301, \
+                    type='each_gauge')
+    plotfigure.clf_each_gauge = True
+
+    plotaxes = plotfigure.new_plotaxes()
+    #plotaxes.axescmd = 'subplot(2,1,2)'
+    plotaxes.title = 'Velocities'
+    plotaxes.afteraxes = add_zeroline
+
+    # Plot velocity as red curve:
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    plotitem.show = True
+    def speed(current_data):
+        from numpy import where, sqrt
+        h = current_data.q[0,:]
+        h = where(h>0.01, h, 1.e6)
+        u = 100. * current_data.q[1,:] / h
+        v = 100. * current_data.q[2,:] / h
+        s = sqrt(u**2 + v**2)
+        return s
+    plotitem.plot_var = speed
+    plotitem.plotstyle = 'k-'
+
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    def uvel(current_data):
+        from numpy import where, sqrt
+        h = current_data.q[0,:]
+        h = where(h>0.01, h, 1.e6)
+        u = 100. * current_data.q[1,:] / h
+        return u
+    plotitem.plot_var = uvel
+    plotitem.plotstyle = 'r-'
+    plotitem.kwargs = {'linewidth':2}
+
+    plotitem = plotaxes.new_plotitem(plot_type='1d_plot')
+    def vvel(current_data):
+        from numpy import where, sqrt
+        h = current_data.q[0,:]
+        h = where(h>0.01, h, 1.e6)
+        v = 100. * current_data.q[2,:] / h
+        return v
+    plotitem.plot_var = vvel
+    plotitem.plotstyle = 'g-'
+    plotitem.kwargs = {'linewidth':2}
+
+    def add_legend_vel(current_data):
+        from pylab import legend
+        # legend(["u","v"],'upper left')
+        legend(['Speed','uvel','vvel'],loc='upper left')
+        add_zeroline(current_data)
+
+    plotaxes.ylimits = [-50,50]
+    plotaxes.afteraxes = add_legend_vel
+
+
+
+    #-----------------------------------------
+
+    # Parameters used only when creating html and/or latex hardcopy
+    # e.g., via clawpack.visclaw.frametools.printframes:
+
+    plotdata.printfigs = True                # print figures
+    plotdata.print_format = 'png'            # file format
+    plotdata.print_framenos = 'all'         # list of frames to print
+    plotdata.print_gaugenos = 'all'          # list of gauges to print
+    plotdata.print_fignos = [4,300,301]           # list of figures to print
+    plotdata.html = True                     # create html files of plots?
+    plotdata.html_homelink = '../README.html'   # pointer for top of index
+    plotdata.latex = False                    # create latex file of plots?
+    plotdata.latex_figsperline = 2           # layout of plots
+    plotdata.latex_framesperline = 1         # layout of plots
+    plotdata.latex_makepdf = False           # also run pdflatex?
+
+    plotdata.kml = True
+    #plotfigure.kml_url = 'http://math.boisestate.edu/~calhoun/visclaw/GoogleEarth/tohoku'
+
+    return plotdata
