db2ff0dc6572b6fa81215c4ab80b80fd8d95347c
Randy LeVeque <rjl@uw.edu>
2023-10-31 16:57:32 -0700

    remove GaugeData1D from data.py, not needed

--- src/python/geoclaw/data.py
+++ src/python/geoclaw/data.py
@@ -13,6 +13,11 @@ Classes representing parameters for GeoClaw runs
  - FGmaxData
  - DTopoData
  - QinitData
+ - SurgeData
+ - MultilayerData
+ - FrictionData 
+ - GridData1D
+ - BoussData1D
 
 :Constants:
 
@@ -742,79 +747,9 @@ class MultilayerData(clawpack.clawutil.data.ClawData):
 
 
 
-#  Gauge data object
 
-#  Gauge data object
-class GaugeData1D(clawpack.clawutil.data.ClawData):
-    r"""
-     Gauge data object for 1d.
-     input specs for gauges are in 1d in setrun.py...output is like that of
-      2d amr (with level=1 and y=0) so that same reading/plotting tools can be used.
-    """
-
-    @property
-    def gauge_numbers(self):
-        if len(self.gauges) == 1:
-            return [self.gauges[0][0]]
-        else:
-            return [gauge[0] for gauge in self.gauges]
-
-    def __init__(self, num_dim=2):
-        super(GaugeData1D,self).__init__()
-
-        self.add_attribute('num_dim',num_dim)
-        self.add_attribute('gauges',[])
-
-    def __str__(self):
-        output = "Gauges: %s\n" % len(self.gauges)
-        for gauge in self.gauges:
-            output = "\t".join((output,"%4i:" % gauge[0]))
-            output = " ".join((output,"%19.10e" % gauge[1]))
-            output = " ".join((output,"%17.10e" % gauge[2]))
-            output = " ".join((output,"%13.6e\n" % gauge[3]))
-        return output
-
-    def write(self,out_file='gauges.data',data_source='setrun.py'):
-        r"""Write out gague information data file."""
-
-
-        # Check to make sure we have only unique gauge numebrs
-        if len(self.gauges) > 0:
-            if len(self.gauge_numbers) != len(set(self.gauge_numbers)):
-                raise Exception("Non unique gauge numbers specified.")
-
-        # Write out gauge data file
-        self.open_data_file(out_file,data_source)
-        self.data_write(name='ngauges',value=len(self.gauges))
-        for gauge in self.gauges:
-            self._out_file.write("%4i %19.10e %19.10e %13.6e  %13.6e\n" % tuple(gauge))
-        self.close_data_file()
-
-    def read(self,data_path="./",file_name='gauges.data'):
-        r"""Read gauge data file"""
-        path = os.path.join(data_path, file_name)
-        gauge_file = open(path,'r')
-
-        # Read past comments and blank lines
-        header_lines = 0
-        ignore_lines = True
-        while ignore_lines:
-            line = gauge_file.readline()
-            if line[0] == "#" or len(line.strip()) == 0:
-                header_lines += 1
-            else:
-                break
-
-        # Read number of gauges, should be line that was last read in
-        num_gauges = int(line.split()[0])
-
-        # Read in each gauge line
-        for n in xrange(num_gauges):
-            line = gauge_file.readline().split()
-            self.gauges.append([int(line[0]),float(line[1]),float(line[2]),
-                                                              float(line[3]),float(line[4])])
-
-        gauge_file.close()
+#  Gauge data object removed, version from amrclaw works in 1d
+#class GaugeData1D(clawpack.clawutil.data.ClawData):
 
 
 class GridData1D(clawpack.clawutil.data.ClawData):
