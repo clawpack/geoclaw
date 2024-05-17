ef4c7781d74c878bbfb806e3d7646f1496b95f94
Katy Barnhart <krbarnhart@usgs.gov>
2024-05-17 16:04:43 -0600

    remove extra words

--- examples/tsunami/chile2010_fgmax-fgout/use_xarray_backends.py
+++ examples/tsunami/chile2010_fgmax-fgout/use_xarray_backends.py
@@ -1,7 +1,7 @@
 try:
     import xarray as xr
 except:
-    'You must install xarray in order to write a use the xarray backends'
+    'You must install xarray in order to use the xarray backends'
     raise
 
 from clawpack.geoclaw.xarray_backends import FGOutBackend, FGMaxBackend
