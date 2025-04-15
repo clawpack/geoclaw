d3aa88703acd1d30501f0761dda7169110b7e8e9
Kyle Mandli <kyle.mandli@gmail.com>
2025-04-15 10:08:10 -0400

    Fix up storm I/O tests

--- examples/storm-surge/isaac/test_storm.py
+++ examples/storm-surge/isaac/test_storm.py
@@ -15,24 +15,25 @@ def test_geoclaw_storm():
     """Test of Storm geoclaw formatted I/O"""
     assert False
 
-def test_OWI_storm():
+@pytest.mark.parametrize("data_file_format", 
+                         ['ascii', 'netcdf', 1, 2, 'nws12', 'nws13'])
+def test_OWI_storm(data_file_format):
     """Test of Storm OWI formatted I/O"""
-    for data_file_format in ['ascii', 'netcdf', 1, 2, 'nws12', 'nws13']:
-        storm = clawpack.geoclaw.surge.storm.Storm()
-        storm.time_offset = np.datetime64("2012-08-29")
-        storm.data_file_format = data_file_format
-        if data_file_format in ['ascii', 1, 'nws12']:
-            storm.file_paths = [Path("storm_1.PRE"), Path("storm_1.WIN"),
-                                Path("storm_2.PRE"), Path("storm_2.WIN")]
-        else:
-            storm.file_paths = [Path("storm.nc")]
-        storm.write(Path("test.storm"), file_format="OWI")
-        read_storm = clawpack.geoclaw.surge.storm.Storm(Path("test.storm"), 
-                                                        file_format="OWI")
-        assert (storm.time_offset == read_storm.time_offset)
-        assert (storm.data_file_format in
-                data_file_format_map[read_storm.data_file_format])
-        assert (storm.file_paths == read_storm.file_paths)
+    storm = clawpack.geoclaw.surge.storm.Storm()
+    storm.time_offset = np.datetime64("2012-08-29")
+    storm.data_file_format = data_file_format
+    if data_file_format in ['ascii', 1, 'nws12']:
+        storm.file_paths = [Path("storm_1.PRE"), Path("storm_1.WIN"),
+                            Path("storm_2.PRE"), Path("storm_2.WIN")]
+    else:
+        storm.file_paths = [Path("storm.nc")]
+    storm.write(Path("test.storm"), file_format="OWI")
+    read_storm = clawpack.geoclaw.surge.storm.Storm(Path("test.storm"), 
+                                                    file_format="OWI")
+    assert (storm.time_offset == read_storm.time_offset)
+    assert (storm.data_file_format in
+            data_file_format_map[read_storm.data_file_format])
+    assert (storm.file_paths == read_storm.file_paths)
 
 if __name__ == '__main__':
     test_OWI_storm()
