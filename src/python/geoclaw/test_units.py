3440822c961402bcbe772bdbfd6396f5007fd142
Kyle Mandli <kyle.mandli@gmail.com>
2025-04-24 11:18:58 -0400

    Add units tests

--- src/python/geoclaw/test_units.py
+++ src/python/geoclaw/test_units.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-import numpy
+import numpy as np
 
 from clawpack.geoclaw.units import units, convert
-
 
 def test_conversions(verbose=False):
     r"""Test unit conversions."""
 
     for (measurement_type, measurement_units) in units.items():
-        value = numpy.pi
+        value = np.pi
         units_list = list(units[measurement_type].keys())
         for i in range(len(units_list)):
             if verbose:
                 print("%s (%s) -> (%s)" % (value, units_list[i - 1], 
                                                   units_list[i]))
             value = convert(value, units_list[i - 1], units_list[i])
-        numpy.testing.assert_allclose([value], [numpy.pi],
+        np.testing.assert_allclose([value], [np.pi],
                       err_msg="Measurement tyep %s failed." % measurement_type)
 
 if __name__ == '__main__':
