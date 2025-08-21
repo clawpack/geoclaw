6b48fd488ee5128be6963abf75804278a350620b
Randy LeVeque <rjl@uw.edu>
2025-08-20 20:52:54 -0700

    fix merge conflicts

--- src/python/geoclaw/kmltools.py
+++ src/python/geoclaw/kmltools.py
@@ -1553,7 +1553,6 @@ def transect2kml(xtrans, ytrans, fname='transect.kml'):
         kml_file.write("\n</Document>\n</kml>")
 
     print('Created ', fname)
-<<<<<<< HEAD
 
 def dtopo_contours2kmz(dtopofiles, dtopo_type=3, dZ_interval=1, dZmax=40,
                        text_label=True, text_x=None, text_y=None,
@@ -1659,11 +1658,9 @@ def dtopo_contours2kmz(dtopofiles, dtopo_type=3, dZ_interval=1, dZmax=40,
 
     if fname_kmz is None:
         fname_kmz = 'dtopo_contours.kmz'
-        
+
     with zipfile.ZipFile(fname_kmz, 'w') as zip:
         for file in files:
             zip.write(file)
         print('Created %s' % os.path.abspath(fname_kmz))
     os.chdir(savedir)
-=======
->>>>>>> master
