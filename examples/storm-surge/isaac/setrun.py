45d51b844d640171c35827b46d162c0bbcf34af4
Kyle Mandli <kyle.mandli@gmail.com>
2026-05-22 11:24:51 -0400

    Add example plotting gauge wind/pressure

--- examples/storm-surge/isaac/setrun.py
+++ examples/storm-surge/isaac/setrun.py
@@ -328,6 +328,9 @@ def setrun(claw_pkg='geoclaw', download_dir=None, storm_dir=None):
     rundata.gaugedata.gauges.append([2, -89.95826667, 29.26460167,
                                      rundata.clawdata.t0,
                                      rundata.clawdata.tfinal])
+    
+    # Record the wind and pressure fields at the gauges
+    rundata.gaugedata.aux_out_fields = [4, 5, 6]
 
     # ------------------------------------------------------------------
     # GeoClaw specific parameters:
